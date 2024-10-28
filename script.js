e6f798ad5beb17d5d6349d87254a169355883815
zouhair <mizouhair4@gmail.com>
2024-10-28 19:26:27 +0100

    end nav

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const Open = document.getElementById('open');
 const Close = document.getElementById('close');
 const nav = document.getElementById('nav');
+const body = document.getElementById('body')
 
 Open.addEventListener("click", () => {
     // Show the navigation and hide the open button
     nav.style.display = "flex";
     Open.style.display = "none";
     Close.style.display = "block";
+    body.classList.add('body')
   });
 Close.addEventListener("click", () => {
     // Hide the navigation and show the open button
     nav.style.display = "none";
     Close.style.display = "none";
     Open.style.display = "block";
+    body.classList.remove('body')
   });
